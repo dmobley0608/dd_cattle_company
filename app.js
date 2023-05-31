dc7fb5f12132968b954e9b57b4e1a30e584a0f8b
dmobley0608 <dmobley0608@gmail.com>
2023-05-30 21:29:09 -0400

    added morgan, bodyparser, colors, and implemented an error handler

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const morgan = require ('morgan')
+const bodyParser = require('body-parser')
+const colors = require('colors')
 const { horsesRouter } = require('./routes/horsesRouter')
 
 //App Config
@@ -7,12 +9,28 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 //Middleware
+    //Morgan
 app.use(morgan('tiny'))
+    //Body Parser
+app.use(bodyParser.json())
 
 //Routes
 app.use('/horses', horsesRouter)
 
 
+
+//Error Handler
+app.use((err, req, res, next)=>{
+   
+    if(!err.status){
+        err.status = 500
+    }
+    if(!err.message){
+        err.message = "Oh No! You found a problem. Please try again."
+    }
+    console.error(`${err.status}-${err.message}`.red)
+    res.status(err.status).send(err.message)
+})
 
 //Open Connection
 app.listen(PORT, ()=>{
--- routes/horsesRouter.js
+++ routes/horsesRouter.js
@@ -4,41 +4,48 @@ const horsesRouter = express.Router()
 //TEMP DATA
 const horses = [{id:'1', name:"Dancer"}, {id:'2', name:'Titus'}, {id:'3', name:'Henry'}]
 
+//Middleware
+const getHorseById=(req, res, next)=>{
+    const {id} = req.params
+    req.horse = horses.filter(horse=>horse.id === id)[0]
+    next()
+}
+
 //Get all Horses
 horsesRouter.get('/', (req, res,next)=>{
     res.send(horses)
 }) 
 
 //Get Horse By Id
-horsesRouter.get('/:id', (req, res, next)=>{
-    const {id} = req.params
-    const horse = horses.filter(horse=>horse.id === id)
-    if(horse){
-        res.send(horse)
+horsesRouter.get('/:id', getHorseById, (req, res, next)=>{    
+    if(req.horse){
+        res.send(req.horse)
     }else{
-        res.status(404).send('Horse Not Found')
+       let error = new Error("Horse Not Found")
+       error.status = 404
+       error.message = "Horse Not Found"
+       throw error
     }
 })
 
 //Add Horse
 horsesRouter.post('/', (req, res,next)=>{
-    const {name, id} = req.query  
+    const {name, id} = req.body  
     const horse = {id, name} 
-    if(horse.name){
+    if(horse.name && horse.id){
         horses.push(horse)
         res.status(201).send(horses)
     }else{
-        res.status(400).send()
+        throw new Error("Invalid Horse Format")
     }
 })
 
 //Update Horse
-horsesRouter.put('/:id', (req, res, next)=>{
-    const {id} = req.params
-    const {name} = req.query   
-    const index = horses.map(horse=>horse.id).indexOf(id)   
+horsesRouter.put('/:id', getHorseById, (req, res, next)=>{    
+    const index = horses.findIndex((horse)=>horse.id === req.params.id)    
+    const {id, name} = req.horse
     if( index !== -1){
-        horses[index] = {id, name}
+        horses[index] = {id:id, ...req.body}
         res.send(horses)
     }else{
         res.status(404).send('Horse Not Found')
@@ -48,7 +55,7 @@ horsesRouter.put('/:id', (req, res, next)=>{
 //Delete Horse
 horsesRouter.delete('/:id', (req, res, next)=>{
     const {id} = req.params 
-    console.log(id) 
+   
     const index = horses.map(horse=>horse.id).indexOf(id)
     console.log(index)
     if(index !== -1){

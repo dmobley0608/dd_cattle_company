024d9435bdbc8d78120aca1f5fe195a8778bf3aa
dmobley0608 <dmobley0608@gmail.com>
2023-05-30 20:03:43 -0400

    implemented morgan

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express')
+const morgan = require ('morgan')
 const { horsesRouter } = require('./routes/horsesRouter')
 
 //App Config
 const app = express()
 const PORT = process.env.PORT || 5000
 
+//Middleware
+app.use(morgan('tiny'))
+
 //Routes
 app.use('/horses', horsesRouter)
+
+
 
 //Open Connection
 app.listen(PORT, ()=>{

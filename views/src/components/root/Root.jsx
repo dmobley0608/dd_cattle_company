d75845587a6a3d6a423be16b05672a44d1f03e7a
dmobley0608 <dmobley0608@gmail.com>
2023-07-10 16:40:28 -0400

    fixed loading screen

--- views/src/components/root/Root.jsx
+++ views/src/components/root/Root.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import Navbar from '../navbar/Navbar'
 import { Outlet } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { selectIsLoading } from '../../features/horses/horsesSlice'
 
 
 export default function Root() {
+  const isLoading = useSelector(selectIsLoading)
   return (
     <>
-      <Navbar />      
-        <Outlet />      
+      <Navbar />
+      {isLoading ? <h1>Loading</h1> :
+
+        <Outlet />
+      }
 
     </>
   )

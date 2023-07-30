adda28b4f7a33eec3d33ad1d637e1f57db86be46
dmobley0608 <dmobley0608@gmail.com>
2023-07-30 08:23:50 -0400

    update to api end points

--- src/features/horses/horsesAPI.js
+++ src/features/horses/horsesAPI.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const apiClient = axios.create(
     {
-    baseURL: `http://localhost:${process.env.REACT_APP_PORT}/api/`
+    baseURL: `/api/`
         
     }
 );

4502b4b2a90f4d262b17e3b20c5fe834fdc1951b
dmobley0608 <dmobley0608@gmail.com>
2023-08-12 07:22:13 -0400

    fixed media upload for heroku

--- server/controllers/media.js
+++ server/controllers/media.js
@@ -4,16 +4,6 @@ const path = require("path")
 const { Horses } = require('../model/horses');
 const { Media } = require('../model/media');
 
-const cloudinary = require('cloudinary').v2
-
-cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET
-})
-
-
-
 
 
 const ik = new ImageKit({
@@ -34,7 +24,7 @@ exports.getMediaByHorseId = async (req, res) => {
         res.status(400).json({ error: err.message })
     }
     // const horses = await Horses.findAll();
-    
+
     // for(let horse of horses){
     //     const media = ik.listFiles({
     //         tags:horse.name
@@ -44,7 +34,7 @@ exports.getMediaByHorseId = async (req, res) => {
     //             await Media.create({horse_id:horse.id, thumbnail:data.thumbnailUrl, ...data}) 
     //             }
     //         }
-           
+
     //     })
     // }
     res.json("finished")
@@ -54,36 +44,28 @@ exports.uploadMedia = async (req, res) => {
     const { horse_id, horse_name } = req.params
     try {
 
-        // for (let media of req.files) {
-        //     await cloudinary.uploader.upload(media.path, { public_id: `double_d_ranch/${horse_name}/${media.filename}` })
-        //         .then(async (res) => {
-        //             await Media.create({ ...res, horse_id: horse_id })
-        //         })
-        // }
         const fs = require('fs-extra')
         const results = []
         const pathUrl = path.join(__dirname, '../uploads')
-        fs.readdir('./uploads', async (err, files) => {
+        fs.readdir(pathUrl, async (err, files) => {
             if (err) return res.send(err)
             for (const file of files) {
-                console.log(file)
-                fs.readFile(`${pathUrl}/${file}`, async(err, data)=>{
+                fs.readFile(`${pathUrl}/${file}`, async (err, data) => {
                     ik.upload({
                         file: data,
                         fileName: file,
                         tags: [horse_name],
-                        folder:`ddc/${horse_name}`
+                        folder: `ddc/${horse_name}`
                     }, (err, result) => {
                         console.log(err)
-                        console.log(result)
                         if (!err) {
-                            Media.create({horse_id:horse_id,thumbnail:result.thumbnailUrl, ...result})
+                            Media.create({ horse_id: horse_id, thumbnail: result.thumbnailUrl, ...result })
                             fs.rm(`./uploads/${file}`, () => { console.log(`Removing ${file}`) })
                             results.push(result)
                         }
                     })
                 })
-              
+
 
             }
         })
@@ -97,19 +79,19 @@ exports.uploadMedia = async (req, res) => {
 }
 
 exports.removeMedia = async (req, res) => {
-    try{
+    try {
         //delete image from image kit
-        ik.deleteFile(req.params.fileId, async function(error, result) {
-            if(error) console.log(error);
+        ik.deleteFile(req.params.fileId, async function (error, result) {
+            if (error) console.log(error);
             else {
                 //delete image from database
-                await Media.destroy({where:{fileId:req.params.fileId}})
+                await Media.destroy({ where: { fileId: req.params.fileId } })
             };
         });
         res.status(200).json("Successfully Deleted Image")
-    }catch(err){
+    } catch (err) {
         res.status(500).json("Error Deleting Image")
     }
-    
+
 }
 

be71d3a1b6b66835bf398250a8f5bf6eabd6ecd8
dmobley0608 <dmobley0608@gmail.com>
2023-08-12 07:44:48 -0400

    fixed media upload for heroku

--- server/routes/mediaRouter.js
+++ server/routes/mediaRouter.js
@@ -2,14 +2,19 @@ const express = require('express');
 const { getMediaByHorseId, uploadMedia, removeMedia } = require('../controllers/media');
 const { auth } = require('../controllers/authentication');
 const router = express();
+const path = require('path');
 
 //Multer For Upload Handling
 const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, '..','uploads'),
+    filename: (req, file, cb) => {             
+        cb(null, file.originalname)
+    }
+})
 
+const upload = multer({storage:storage})
 
-
-
-const upload = multer({dest:'uploads/'})
 
 router.get('/:id', getMediaByHorseId)
 

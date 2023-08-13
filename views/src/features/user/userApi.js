4985ab1482698265cca6a485205fc0e46b7e0b29
dmobley0608 <dmobley0608@gmail.com>
2023-08-13 06:09:53 -0400

    file folder cleanup

--- views/src/features/user/userApi.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const apiClient = axios.create(
-    {
-    baseURL: 'https://ddcattle-backend-9cb82b066a36.herokuapp.com/'
-    }
-);
-
-export const emailLogin = async(email, password)=>apiClient.post('user/login', {email:email, password:password})

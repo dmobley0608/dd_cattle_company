e03e4b7438afdb01deb6bc6c77db12dda25264d6
dmobley0608 <dmobley0608@gmail.com>
2023-08-16 23:07:35 -0400

    fixed admin page loading error

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,18 +1,22 @@
 
-import React, {  useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import AdminMedia from './AdminMedia'
 import AdminRecords from './AdminRecords'
 import HorseForm from './forms/HorseForm'
-import {  useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { selectUser } from '../../features/user/userSlice'
-import {  selectHorse } from '../../features/horses/horsesSlice'
+import { loadHorses, selectHorse, selectIsLoading } from '../../features/horses/horsesSlice'
 import './admin.styles.css'
 import { Tab, Tabs, ThemeProvider } from '@mui/material'
+import Loading from '../../components/loading/Loading'
 import { whiteBlack } from '../../components/themes/themes'
 import SideMenu from './components/side-menu/SideMenu'
 
 
-export default function Admin() {    
+export default function Admin() {
+    const isLoading = useSelector(selectIsLoading)
+    const dispatch = useDispatch();
+   
     const horse = useSelector(selectHorse)
     const [activeScreen, setActiveScreen] = useState('about')
     const user = useSelector(selectUser)
@@ -30,31 +34,36 @@ export default function Admin() {
         setActiveScreen(newValue);
     };
 
-    useEffect(()=>{
+    useEffect(() => {
+        dispatch(loadHorses())
         setActiveScreen('about')
-    },[horse])
+    }, [])
 
     return (
 
         <ThemeProvider theme={whiteBlack} >
             <div id="admin">
-                 {/* Side Menu */}
-            <SideMenu id="side-menu" activeStyle={activeStyle} />
-            {/* Horse Information */}
+                {/* Side Menu */}
+                <SideMenu id="side-menu" activeStyle={activeStyle} />
+                {/* Horse Information */}
 
-            <div className='w_100 center'>
-                {!horse.name && <h2>New Horse Form</h2>}
-                {horse.name && <Tabs value={activeScreen} onChange={handleChange}  indicatorColor='primary'  sx={{mb:2, mt:5}}>
-                    <Tab value='about' label="About" >About</Tab>
-                    <Tab value='records' label="Records" >Medical Records</Tab>
-                    <Tab value='media' label="Media">Media</Tab>
-                </Tabs>}
-                {activeScreen === 'about' && <HorseForm />}
-                {activeScreen === 'media' && <AdminMedia user={user} />}
-                {activeScreen === 'records' && <AdminRecords user={user} />}
+                <div className='w_100 center'>
+                    {!horse.name && <h2>New Horse Form</h2>}
+                    {horse.name && <Tabs value={activeScreen} onChange={handleChange} indicatorColor='primary' sx={{ mb: 2, mt: 5 }}>
+                        <Tab value='about' label="About" >About</Tab>
+                        <Tab value='records' label="Records" >Medical Records</Tab>
+                        <Tab value='media' label="Media">Media</Tab>
+                    </Tabs>}
+                    {isLoading ? <Loading/> :
+                        activeScreen === 'about' ? <HorseForm />
+                            :
+                            activeScreen === 'media' ? <AdminMedia user={user} />
+                                :
+                                activeScreen === 'records' && <AdminRecords user={user} />
+                    }
+                </div>
             </div>
-            </div>
-           
+
 
         </ThemeProvider>
     )
--- src/pages/admin/components/side-menu/SideMenu.jsx
+++ src/pages/admin/components/side-menu/SideMenu.jsx
@@ -1,31 +1,35 @@
 import React from 'react'
 import { useDispatch, useSelector, } from 'react-redux'
-import { getHorseById, selectAllHorses, selectHorse } from '../../../../features/horses/horsesSlice'
+import { getHorseById, getHorseByName, selectAllHorses, selectHorse, selectIsLoading } from '../../../../features/horses/horsesSlice'
 
 import './SideMenu.styles.css'
 import { Tab, Tabs } from '@mui/material'
 
 
-export default function SideMenu({activeStyle}) {    
-
+export default function SideMenu({ activeStyle }) {
   
-  const dispatch = useDispatch()  
+
+  const dispatch = useDispatch()
   const horses = useSelector(selectAllHorses)
   const horse = useSelector(selectHorse)
- 
+
 
 
 
   return (
     <div >
-      <select className="dropdown" onChange={(e)=>dispatch(getHorseById(e.target.value))}>
-        <option>Select Horse</option>
-        {Object.values(horses).map(horse => <option key={horse.id} value={horse.id}>{horse.name}</option>)}
-      </select>
-      <Tabs id='side-menu' orientation='vertical' value={horse.id? horse.id : -1} onChange={(e, value)=>{dispatch(getHorseById(value))}}  indicatorColor='secondary'  sx={{m:5, borderRight:1, padding:2}}>
-      <Tab className='menu' value={null}  label="+ Add Horse"  />
-        {Object.values(horses).map(horse => <Tab className='menu' value={horse.id} key={horse.id} label={horse.name}  />)}
-      </Tabs>
+      
+     
+          <select className="dropdown" onChange={(e) => dispatch(getHorseById(e.target.value))}>
+            <option>Select Horse</option>
+            {Object.values(horses).map(horse => <option key={horse.id} value={horse.id}>{horse.name}</option>)}
+          </select>
+          <Tabs id='side-menu' orientation='vertical' value={horse.name ? horse.name : null} onChange={(e, value) => { dispatch(getHorseByName(value)) }} indicatorColor='secondary' sx={{ m: 5, borderRight: 1, padding: 2 }}>
+            <Tab className='menu' value={null} label="+ Add Horse" />
+            {Object.values(horses).map(horse => <Tab className='menu' value={horse.name} key={horse.id} label={horse.name} />)}
+          </Tabs>
+       
+    
     </div>
   )
 }

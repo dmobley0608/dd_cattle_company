e3e5029189a90ec77edd7e84087365361cdc9307
dmobley0608 <67826887+dmobley0608@users.noreply.github.com>
2023-08-18 09:49:27 -0400

    update to errer handling

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ const Authenticater = ({ children }) => {
 //Create Router
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Root />}>
+    <Route path="/" element={<Root />} errorElement={<ErrorHandler message={"Dang! You found a problem!"}/>}>
       <Route path="/" element={<Homepage />} />
       <Route path="/admin" element={<Authenticater><Admin /></Authenticater>} />
       <Route path="/login" element={<Login />} />

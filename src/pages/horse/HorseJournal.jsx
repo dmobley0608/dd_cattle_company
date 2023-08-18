1baa8034fe4d8256f346f224c8a5fd00bffad060
dmobley0608 <67826887+dmobley0608@users.noreply.github.com>
2023-08-18 09:10:07 -0400

    code restructure for horseJournal

--- src/pages/horse/HorseJournal.jsx
+++ src/pages/horse/HorseJournal.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect, useSelector, useDispatch } from 'react';
+import { useParams } from 'react-router-dom'
+import { loadHorse, selectIsLoading } from '../../features/horse/horseSlice'
+import JournalCard from './components/journalCard/JournalCard'
+import Loading from '../../components/loading/Loading'
 
-export default function HorseJournal() {
-  return (
-    <div>HorseJournal</div>
-  )
+export default Journal = () => {
+    const isLoading = useSelector(selectIsLoading)
+    const { horseName } = useParams()
+    const horse = useSelector(selectHorse)
+    const dispath = useDispatch()
+
+    useEffect(() => {
+        dispatchEvent(loadHorse())
+    }, [])
+    return (
+        <>
+            <h1>Riding Journal For {horse.name}</h1>
+            {isLoading ? <Loading /> :
+
+                horse.Journal.map(journal => (<JournalCard journal={journal} />))
+            }
+        </>
+    )
 }

11609a38431d3da49796c94812c5839edced712e
dmobley0608 <67826887+dmobley0608@users.noreply.github.com>
2023-08-18 09:06:13 -0400

    Journal Page for horse

---
+++ src/pages/journal/JournalCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default JournalCard = (journal) => {
+    return (
+        <>
+            <h2>{journal.title}</h2>
+            <h3>{journal.date}</h3>
+            <h3>{journal.body}</h3>
+            <h3>-{journal.user}</h3>
+        </>
+    )
+}
+

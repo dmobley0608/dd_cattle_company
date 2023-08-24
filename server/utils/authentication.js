bfa74c295263b0a783054458df6b4a5fb2c438ad
dmobley0608 <dmobley0608@gmail.com>
2023-08-24 07:12:41 -0400

    initial commit

--- server/utils/authentication.js
+++ server/utils/authentication.js
@@ -3,6 +3,8 @@ const passport = require('passport');
 const { compareHash } = require('./helper');
 const { User } = require('../model/user');
 const LocalStrategy = require("passport-local");
+const GoogleStrategy = require('passport-google-oidc')
+//Local Login
 passport.use(new LocalStrategy(
     {usernameField:'email'},
     async function (username, password, done) {       
@@ -31,5 +33,34 @@ passport.deserializeUser(function(user, cb){
         cb(null,  {username: user.username, role:user.role})  
     })
 })
+
+//Google Login
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/oauth/google',
+    scope: ['profile', 'email']
+},
+async function verify(issuer, profile, cb){   
+    
+    let user = await User.findOne({where:{id:profile.id}})
+    
+    if(!user){
+       user= await User.create({id:profile.id, username: profile.displayName, role:'user', email: profile.emails[0].value})        
+    }       
+    return cb(null, user)
+}
+))
+passport.serializeUser(function(user, cb){
+    process.nextTick(()=>{        
+        cb(null, {first_name: user.first_name, role:user.role})
+    })
+});
+passport.deserializeUser(function(user, cb){
+    process.nextTick(()=>{
+        cb(null, {first_name: user.first_name, role: user.role})  
+    })
+})
+
 
 module.exports = passport

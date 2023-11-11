56e47f283925d2ece57cf3c6b586c855f32047e1
dmobley0608 <dmobley0608@gmail.com>
2023-11-11 07:25:55 -0500

    added weight and height to horse page

--- src/pages/horse/AboutHorse.jsx
+++ src/pages/horse/AboutHorse.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import styles from './Horse.module.css'
 import { useSelector } from "react-redux";
 import { selectHorse } from "../../features/horses/horsesSlice";
@@ -6,24 +6,45 @@ import { selectHorse } from "../../features/horses/horsesSlice";
 
 export default function AboutHorse() {
   const horse = useSelector(selectHorse)
-
+  const [image, setImage] = useState(null)
+  const [weight, setWeight] = useState(null)
+  const [height, setHeight] = useState(null)
   //Random Image 
-  const getRandomImage = () => {
-    
-      let images = horse.Media.filter(img => img.fileType === 'image')
-      if (images.length >= 1) {
-        const randomNumber = Math.floor(Math.random() * images.length)
-        return <img className={styles['main-horse-image']} src={`${images[randomNumber].url}`} alt="horse" />
-      }
-      return <h3>Image Coming Soon</h3>
-    
+  const getRandomImage = useCallback(() => {
+    let images = horse.Media.filter(img => img.fileType === 'image')
+    if (images.length >= 1) {
+      const randomNumber = Math.floor(Math.random() * images.length)
+      return <img className={styles['main-horse-image']} src={`${images[randomNumber].url}`} alt="horse" />
+    }
+    return <h3>Image Coming Soon</h3>
+  }, [horse.Media])
 
-  }
+  // Weight and Height
+  const getHorseWeight = useCallback(() => {
+    const weightRecords = horse.MedicalRecords.filter(rec => rec.weight > 0)
+    weightRecords.sort((a, b) => a.id > b.id)
+    const weight = weightRecords[0] ? ` ${weightRecords[0].weight} lbs` : null
+    setWeight(weight)
 
+  }, [horse.MedicalRecords])
+
+  const getHorseHeight = useCallback(() => {
+    const heightRecords = horse.MedicalRecords.filter(rec => rec.height > 0)
+    heightRecords.sort((a, b) => a.id > b.id)
+    const height = heightRecords[0] ? ` ${heightRecords[0].height} hands` : null
+    setHeight(height)
+  }, [horse.MedicalRecords])
+
+
+  useEffect(() => {
+    setImage(getRandomImage())
+    getHorseWeight()
+    getHorseHeight()
+  }, [getRandomImage, getHorseHeight, getHorseWeight])
 
 
   return (
-    <div className={'animate__animated animate__fadeIn '+styles['about']}>
+    <div className={'animate__animated animate__fadeIn ' + styles['about']}>
       <h1>{horse.name}</h1>
 
       {/* General Information */}
@@ -38,8 +59,10 @@ export default function AboutHorse() {
             new Date().getFullYear() - horse.birth_date.split("-")[0]
           }
           </p>
+          {height && <p>Height: {height}</p>}
+          {weight && <p>Weight: {weight}</p>}          
         </div>
-        <div className={styles["col"]}>{getRandomImage()} </div> 
+        <div className={styles["col"]}>{image} </div>
       </div>
       <hr /> {/* Bio */}
       <div>
